22b06ea467732c423edd985d6613413274eceae2
bsvisser <brentsvisser@gmail.com>
2022-01-25 22:28:09 +0100

    Update passive_solver.py

--- passive_solver.py
+++ passive_solver.py
@@ -94,7 +94,7 @@ if st.checkbox("Verplichte relics?"):   #multiselect
 if st.button("Run"):
     st.sidebar.table(allrelics)
 
-    comblist = [value for value in allrelics if value in masterlist]
+    comblist = [value for value in allrelics if value in flat_list]
     st.write(comblist)
     combinations = itertools.combinations(comblist, numrelics)
     
