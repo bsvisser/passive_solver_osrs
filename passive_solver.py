24e150a87654b8a6afc6d12f46880a51f9e988d1
bsvisser <brentsvisser@gmail.com>
2022-01-26 16:50:59 +0100

    Update passive_solver.py

--- passive_solver.py
+++ passive_solver.py
@@ -81,7 +81,7 @@ masterlist = []
 
 for tp in targetpassives:
     withnan_list = list(set(df[tp].values[1:]))
-    nonanlist = [i for i in withnan_list if i != "nan"]
+    nonanlist = [i for i in withnan_list if i != "NaN"]
     st.write(nonanlist)
     masterlist.append(nonanlist)
 flat_list =[item for sublist in masterlist for item in sublist]
